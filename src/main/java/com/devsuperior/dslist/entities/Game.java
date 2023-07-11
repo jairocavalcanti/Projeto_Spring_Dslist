52e582c2be03d1c16277d6ed58271d16c5b1e746
Jairo <jairoregiscavalcanti@gmail.com>
2023-07-11 01:24:50 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -30,27 +30,39 @@ public class Game {
     private Integer year;
     
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
+  
     /*  
     'imgUrl' segue o padrao 'camel case' inciando com caracteres minusculos
      Ao inicio de outra palavra sera inserido um caracter maiusculo.
      ajudando assim na legibilidade
     */
     private String imgUrl;
+  
+    /*
+    Definindo 'shortdescription' e 'longdescription' como tipos texto no banco de dados 
+    Podendo assim alocar uma maior quantidade de caracteres, como o texto 'lorem ipsum'
+    definido como padrao neste campo
+    */
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -91,11 +103,19 @@ public class Game {
     }
 
     public String getPlatform() {
-        return platform;
+        return platforms;
     }
 
     public void setPlatform(String platform) {
-        this.platform = platform;
+        this.platforms = platform;
+    }
+
+     public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
@@ -152,6 +172,8 @@ public class Game {
             return false;
         return true;
     }
+
+   
 
 
 }
